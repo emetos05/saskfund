2e21534bccaa0140be1d3c7030b59cd5ff659771
Daniel Emehinola <dan.emehin@gmail.com>
2023-11-21 14:18:51 -0600

    logo and favicon update

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -2,14 +2,13 @@
 
 import React from "react";
 import Link from "next/link";
-
-import { FcMoneyTransfer } from "react-icons/fc";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav className="flex space-x-6 px-5 h-14 items-center bg-gradient-radial from-sky-300 to-sky-700">
+    <nav className="flex space-x-6 px-5 h-20 items-center bg-gradient-radial from-sky-300 to-sky-700">
       <Link href="/">
-        <FcMoneyTransfer />
+        <Image src="/images/logo.png" alt="logo" height={70} width={70} />
       </Link>
     </nav>
   );

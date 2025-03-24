5d76ec154eaeae089d2d9ed6551e6434b3134f6b
Daniel Emehinola <dan.emehin@gmail.com>
2025-03-23 21:07:42 -0400

    hover animation with vibe coding

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -7,7 +7,7 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <nav className="flex space-x-6 px-5 h-20 items-center bg-gradient-radial from-sky-300 to-sky-700">
-      <Link href="/">
+      <Link href="/" className="transform transition-transform duration-200 hover:scale-110">
         <Image src="/images/logo.png" alt="logo" height={70} width={70} />
       </Link>
     </nav>
